fdf4abe905272779fde0a83afada8c5f120a8e32
shubhmindrops01 <59384019+shubhmindrops01@users.noreply.github.com>
2020-04-02 18:30:32 +0530

    UPDATE : vibrate and finish button color updated

--- six_feet/app/src/main/java/com/mindrops/sixfeet/ARActivity.java
+++ six_feet/app/src/main/java/com/mindrops/sixfeet/ARActivity.java
@@ -1,7 +1,11 @@
 package com.mindrops.sixfeet;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -249,6 +253,15 @@ public class ARActivity extends AppCompatActivity implements Scene.OnUpdateListe
     }
 
     private void showPopup() {
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        // Vibrate for 500 milliseconds
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            //deprecated in API 26
+            v.vibrate(500);
+        }
+
         distanceTv.setVisibility(View.GONE);
         warningTv.setVisibility(View.GONE);
         descrTv.setVisibility(View.VISIBLE);
